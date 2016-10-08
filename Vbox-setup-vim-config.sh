d3a3ad1572b3e16e838e9ca34a20753633c0d932
Thad Brown <tcb@thadbrown.com>
2016-10-08 18:39:39 +0000

    add cmake

--- Vbox-setup-vim-config.sh
+++ Vbox-setup-vim-config.sh
@@ -3,7 +3,7 @@
 sudo apt-get -y install libncurses5-dev libgnome2-dev libgnomeui-dev \
 libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
 libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-python3-dev lua5.1 lua5.1-dev git curl \
+python3-dev lua5.1 lua5.1-dev git curl cmake \
 build-essential module-assistant tree linux-headers-$(uname -r) \
 tmux autoconf bison libssl-dev libyaml-dev libreadline6-dev zlib1g-dev \
 libffi-dev libgdbm3 libgdbm-dev
