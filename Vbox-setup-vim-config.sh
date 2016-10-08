f81795e30deae284d1abae494aa095c52589a27d
Thad Brown <tcb@thadbrown.com>
2016-10-08 16:58:12 +0000

    update forgotten linux-headers

--- Vbox-setup-vim-config.sh
+++ Vbox-setup-vim-config.sh
@@ -4,4 +4,4 @@ sudo apt-get -y install libncurses5-dev libgnome2-dev libgnomeui-dev \
 libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
 libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
 python3-dev ruby ruby-dev lua5.1 lua5.1-dev git ri bundler \
-build-essential module-assistant tree 
+build-essential module-assistant tree linux-headers-$(uname -r)
