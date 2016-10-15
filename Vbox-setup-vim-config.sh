c7e69cbde2b50993313619961c3eb586e919c273
tcb <tcb@debDocker>
2016-10-15 16:41:59 -0500

    Adds meld and apt-transport-htpps

--- Vbox-setup-vim-config.sh
+++ Vbox-setup-vim-config.sh
@@ -3,7 +3,8 @@
 sudo apt-get -y install libncurses5-dev libgnome2-dev libgnomeui-dev \
 libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
 libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-python3-dev lua5.1 lua5.1-dev git curl cmake \
+python3-dev lua5.1 lua5.1-dev git curl cmake meld \
 build-essential module-assistant tree linux-headers-$(uname -r) \
 tmux autoconf bison libssl-dev libyaml-dev libreadline6-dev zlib1g-dev \
-libffi-dev libgdbm3 libgdbm-dev
+libffi-dev libgdbm3 libgdbm-dev apt-transport-https
+
